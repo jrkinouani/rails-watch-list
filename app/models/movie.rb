9bad6ea308094d95c88d6a61bd7d88a85a31c9ff
jean-rodriguez kinouani <kinouani@gmail.com>
2022-08-19 15:31:01 +0200

    add image

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
-	has_many :lists
-	has_many :bookmarks 
+	has_many :lists, through: :bookmarks
+	has_many :bookmarks, dependent: :destroy
 	validates :title, uniqueness: true
 	validates :title, :overview, presence:true
 end
